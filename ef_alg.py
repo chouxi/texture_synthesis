57c542b4ced7db94179a010eeb74801b3f9c4789
chouxi <qizheng1993hit@gmail.com>
2017-03-22 15:57:00 -0400

    gaussain2d mask

--- ef_alg.py
+++ ef_alg.py
@@ -13,6 +13,17 @@
 import numpy as np
 from skimage import io
 import matplotlib.pyplot as plt
+def gaussian2D(size, sigma = 1, center=None):
+    x = np.arange(0, size, 1, float)
+    y = x[:,np.newaxis]
+
+    if center is None:
+        x0 = y0 = size // 2
+    else:
+        x0 = center[0]
+        y0 = center[1]
+    return np.exp(-4*np.log(2) * ((x-x0)**2 + (y-y0)**2) / sigma**2)
+
 def get_unfilled_neighbor(visited_mat):
     direction = [(0,1), (0,-1), (-1,0), (1,0)]
     x, y = visited_mat.shape
@@ -34,15 +45,27 @@ def get_neighborwind(image, window_size, pixel):
     if window_size & 1 == 0:
         window_size += 1
     margin = window_size / 2
-    return image[max(0, pixel[0] - margin):min(x - 1, pixel[0] + margin), max(0, pixel[1]-margin):min(y - 1,pixel[1]+margin)]
+    #return image[max(0, pixel[0] - margin):min(x - 1, pixel[0] + margin), max(0, pixel[1]-margin):min(y - 1,pixel[1]+margin)]
+    return [max(0, pixel[0] - margin),min(x - 1, pixel[0] + margin), max(0, pixel[1]-margin),min(y - 1,pixel[1]+margin)]
     
-def grow_image(sample, image, visited_mat, window):
+def find_matches(template, sample,visited_mat, window_size):
+    sigma = 0.3
+    valid_mask = visited_mat[template[0]:template[1], template[2]: template[3]]
+    #sigma_mat = np.ones((2,2)) *sigma
+    #gauss_mask = np.random.multivariate_normal([window_size/2+1,window_size/2+1],sigma_mat, (window_size,window_size))
+    gauss_mask = gaussian2D(window_size, sigma)
+
+def grow_image(sample, image, visited_mat, window_size):
     while 1:
         pixel_list = get_unfilled_neighbor(visited_mat)
         if len(pixel_list) == 0:
             break
         for pixel in pixel_list:
-            tmpelate = get_neighborwind(image, window, pixel)
+            template = get_neighborwind(image, window_size, pixel)
+            find_matches(template, sample, visited_mat, window_size)
+            visited_mat[pixel[0],pixel[1]] = 1
+            break
+        break
 
 def do_efros(sample, new_x, new_y, window_size):
     size = sample.shape
@@ -57,8 +80,8 @@ def do_efros(sample, new_x, new_y, window_size):
     image[start_x: (start_x + size[0]), start_y:(start_y + size[1])] = sample
     visited_mat[start_x: (start_x + size[0]), start_y:(start_y + size[1])] = 1
     grow_image(sample, image, visited_mat, window_size)
-    io.imshow(image)
-    io.show()
+    #io.imshow(image)
+    #io.show()
 
 sample = io.imread('./pics/T1.gif')
 do_efros(sample, 200, 200, 5)
