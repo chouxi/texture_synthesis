3b8ac552b933ef46a468058eca8b2618f6f7a207
chouxi <qizheng1993hit@gmail.com>
2017-04-05 12:10:35 -0400

    update readme, and change some test for testing

--- README.md
+++ README.md
@@ -23,3 +23,63 @@ There are 3 classes,
 - criminis_algorithm
 
   The implementation of part3
+
+## How to run
+  The main test file is test.py.
+
+  If you want to run, you need to uncomment the comment part in the test.py.
+  
+  The basic idea to run code is first create an object of base_operation with file_name and the window size. Then create different objects for different part, then call the corresponding functions.
+
+- Part 1:
+	```python
+    base_op =  base_operation('./pics/tT1.gif', 11)
+    efros_obj = efros_algorithm(base_op)
+    efros_obj.efros_synthesis(200, 200)
+	```
+	Only change the './pics/tT1.gif' to different files and 11 to different window size.
+
+- Part 2:
+	```python
+    base_op =  base_operation('./pics/test_im1.bmp', 11)
+    efros_obj = efros_algorithm(base_op)
+    efros_obj.efros_impainting()
+	```
+
+	Only change the './pics/test_im1.bmp' to different files and 11 to different window size.
+
+- Part 3:
+
+	Criminis
+	```python
+    base_op =  base_operation('./pics/test_im3.jpg', 9)
+    # crinimis part
+    criminis_obj = criminis_algorithm(base_op)
+    # people
+    criminis_obj.remove_blocks([(352,485,222,253)])
+    criminis_obj.do_criminis()
+	```
+	Change list in the remove_blocks for different part to remove. Already listed the listes with comment in the code.
+
+	Efros
+	```python
+    base_op =  base_operation('./pics/test_im3.jpg', 9)
+    efros_obj = efros_algorithm(base_op)
+    #person
+    efros_obj.efros_removal([(352,485,222,253)])
+	```
+	similar to criminis.
+
+Very easy to run the code based on the OO.
+
+- Part 4:
+	
+	run PatchBasedSynthesis.py.
+	
+	Because cv2 cannot open gif files, I resaved the gifs to jpgs.
+
+	`PatchBasedSynthesis.py /image/source.jpg Patch_Size Overlap_Width Initial_Threshold_error`
+
+	For example,
+	
+	`python PatchBasedSynthesis.py ./pics/T1.jpg 24 4 5.0`
--- test.py
+++ test.py
@@ -16,29 +16,46 @@ from ef_alg import *
 from criminis_alg import *
 
 if __name__ == '__main__':
+    # Part 1
+    '''
+    base_op =  base_operation('./pics/T1.gif', 11)
+    efros_obj = efros_algorithm(base_op)
+    efros_obj.efros_synthesis(200, 200)
+    '''
+
+    # Part 2
+
     '''
     base_op =  base_operation('./pics/test_im1.bmp', 11)
-    #efros_obj = efros_algorithm(base_op)
-    #efros_obj.efros_synthesis(200, 200)
     efros_obj = efros_algorithm(base_op)
     efros_obj.efros_impainting()
     '''
     '''
+    base_op =  base_operation('./pics/test_im2.bmp', 11)
+    efros_obj = efros_algorithm(base_op)
+    efros_obj.efros_impainting()
+    '''
+    # Part 3
+    '''
     base_op =  base_operation('./pics/test_im3.jpg', 9)
+    # crinimis part
     criminis_obj = criminis_algorithm(base_op)
     # people
     #criminis_obj.remove_blocks([(352,485,222,253)])
     # sign
     #criminis_obj.remove_blocks([(513,567,770,830),(566,664,788, 803)])
-    # pole
-    criminis_obj.remove_blocks([(630, 664,3,390), (600, 630, 3, 435), (570,600,95,490), (540,570, 190,530),(510,540, 283, 570), (480, 510, 390, 610), (465,480, 465, 630),(440,465, 515,660)])
-    criminis_obj.do_criminis()
+    # ground
+    #criminis_obj.remove_blocks([(630, 664,3,390), (600, 630, 3, 435), (570,600,95,490), (540,570, 190,530),(510,540, 283, 570), (480, 510, 390, 610), (465,480, 465, 630),(440,465, 515,660)])
+    #criminis_obj.do_criminis()
+    '''
+    # efors part
     '''
     base_op =  base_operation('./pics/test_im3.jpg', 9)
     efros_obj = efros_algorithm(base_op)
-    # person
-    # efros_obj.efros_removal([(352,485,222,253)])
-    # pole
-    # efros_obj.efros_removal([(630, 664,3,390), (600, 630, 3, 435), (570,600,95,490), (540,570, 190,530),(510,540, 283, 570), (480, 510, 390, 610), (465,480, 465, 630),(440,465, 515,660)])
-    # sign
-    efros_obj.efros_removal([(513,567,770,830),(566,664,788, 803)])
+    #person
+    #efros_obj.efros_removal([(352,485,222,253)])
+    #ground
+    #efros_obj.efros_removal([(630, 664,3,390), (600, 630, 3, 435), (570,600,95,490), (540,570, 190,530),(510,540, 283, 570), (480, 510, 390, 610), (465,480, 465, 630),(440,465, 515,660)])
+    #sign
+    #efros_obj.efros_removal([(513,567,770,830),(566,664,788, 803)])
+    ''
